b1aaf36ff8512ecde34751a857865db3aefde1de
Vinícius <viniciuslln@outlook.com>
2017-06-04 16:53:13 -0300

    - Criando população(conferir), gerando amostras e calculando medias

--- Estatistica/MainWindow.xaml.cs
+++ Estatistica/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Estatistica
 {
@@ -21,22 +13,121 @@ namespace Estatistica
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double _nextGauss;
+        private bool _available;
+        private Random _rng = new Random();
+
+        int tamPopulacao = 1000;
+        int media = 12;
+        int variancia = 9;
+        double erro;
+
+        double[] populacao;
+        List<double[]> amostras;
+
         public MainWindow()
         {
             InitializeComponent();
 
             //Inicializa área do gráfico
-            ChartArea aChartArea = new ChartArea();
+            ChartArea aChartArea = new ChartArea("Intervalos");
             Chart1.ChartAreas.Add(aChartArea);
 
-            var xExemplo = new double[] { 2, 5.8, 6, 9.7, 15, 17.8 };
-            var yExemplo = new double[] { 4, 12, 8, 11.9, 15.8, 10 };
-            var erroExemplo = new double[] { 5, 5, 5, 5, 5, 5 };
+            //Inicializando população
+            erro = 1.96 * (Math.Sqrt(variancia) / Math.Sqrt(25));
+            gerarPopulacao();
+            coletarAmostras();
+            gerarGrafico();
 
-            DesenharGrafico(xExemplo, yExemplo, erroExemplo);
+
+        }
+        public double RandomGauss(double media, double desvioPadrao)
+        {
+            if (_available)
+            {
+                _available = false;
+                return media + desvioPadrao * _nextGauss;
+            }
+
+            double u1 = _rng.NextDouble();
+            double u2 = _rng.NextDouble();
+            double temp1 = Math.Sqrt(-2.0 * Math.Log(u1));
+            double temp2 = 2.0 * Math.PI * u2;
+
+            _nextGauss = temp1 * Math.Sin(temp2);
+            _available = true;
+            var toReturn = temp1 * Math.Cos(temp2);
+            return media + desvioPadrao * toReturn;
         }
 
-        public void DesenharGrafico(double [] x, double [] y, double [] erro)
+        private void coletarAmostras()
+        {
+            amostras = new List<double[]>();
+            Random random = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                HashSet<int> randomNumbers = new HashSet<int>();
+                for (int j = 0; j < 25; j++)
+                    while (!randomNumbers.Add(random.Next(0,999))) ;
+                var amostra = new double[25];
+                var numeros = randomNumbers.ToList();
+                for (int j = 0; j < 25; j++)
+                    amostra[j] = populacao[numeros[j]];
+                amostras.Add(amostra);
+                var item = new TreeViewItem()
+                {
+                    Header = "Amostra " + (i + 1),
+                };
+                item.PreviewMouseLeftButtonDown += clickLista;
+                item.Items.Add(new ListView()
+                {
+                    ItemsSource = amostra
+                });
+                TreeViewAmostras.Items.Add(item);
+            }
+        }
+
+        private void clickLista(object sender, MouseButtonEventArgs e)
+        {
+            int i = TreeViewAmostras.Items.IndexOf(sender as TreeViewItem);
+            var amostra = amostras[i];
+
+            var mediaAmostral = calculaMedia(amostra);
+
+            double erroSuperior = mediaAmostral + erro;
+            double erroInferior = mediaAmostral - erro;
+
+            StackPanelInformacoesAmostra.Children.Clear();
+            StackPanelInformacoesAmostra.Children.Add(new TextBlock()
+            {
+                Text = "Média da Amostra: " + mediaAmostral
+            });
+            StackPanelInformacoesAmostra.Children.Add(new TextBlock()
+            {
+                Text = "Erro Superior: " + erroSuperior
+            });
+            StackPanelInformacoesAmostra.Children.Add(new TextBlock()
+            {
+                Text = "Erro ingerior: " + erroInferior
+            });
+        }
+
+        public void gerarPopulacao()
+        {
+            populacao = new double[tamPopulacao];
+            for (int i = 0; i < tamPopulacao; i++)
+            {
+                populacao[i] = RandomGauss(media, Math.Sqrt(variancia));
+            }
+            ListViewPopulação.ItemsSource = populacao;
+            var mediaCalculada = calculaMedia(populacao);
+            StackPanelInformacoes.Children.Add(new TextBlock()
+            {
+                Text = "Média da População: " + mediaCalculada
+            });
+        }
+
+        public void gerarGrafico()
         {
             Series errorBarSeries = new Series("ErrorBar")
             {
@@ -45,14 +136,45 @@ namespace Estatistica
                 ShadowOffset = 1,
             };
             errorBarSeries["ErrorBarCenterMarkerStyle"] = "Circle";
-            errorBarSeries["ErrorBarType"] = "StandardDeviation";
- 
-            for (int i = 0; i<x.Length; i++)
+           // errorBarSeries["ErrorBarType"] = "StandardDeviation";
+
+
+            for (int x = 1; x <= amostras.Count; x++)
             {
-                errorBarSeries.Points.AddXY(x[i], y[i], y[i] - erro[i], y[i] + erro[i]);
+                var amostra = amostras[x-1];
+                
+                var mediaAmostral = calculaMedia(amostra);
+
+                double erroSuperior = mediaAmostral + erro;
+                double erroInferior = mediaAmostral - erro;
+                
+                errorBarSeries.Points.AddXY(x, mediaAmostral, erroInferior, erroSuperior);
+
             }
+
             Chart1.Series.Clear();
             Chart1.Series.Add(errorBarSeries);
+
+            StripLine stripline = new StripLine()
+            {
+                Interval = 0,
+                IntervalOffset = 12,
+                StripWidth = 0.1,
+                BackColor = System.Drawing.Color.Purple,
+                BorderDashStyle = ChartDashStyle.Dot,
+                Text = "μ"
+            };
+            Chart1.ChartAreas["Intervalos"].AxisY.StripLines.Add(stripline);
+
         }
+
+        double calculaMedia(double[] l)
+        {
+            var media = 0.0;
+            foreach (var x in l)
+                media += x;
+            return media / l.Length;
+        }
+
     }
 }
