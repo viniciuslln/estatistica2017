906e480825e621ba009f41b44ef6ef684466fc68
viniciuslln <viniciuslln@outlook.com>
2017-06-05 11:59:22 -0300

    Update MainWindow.xaml.cs
    Comentários

--- Estatistica/MainWindow.xaml.cs
+++ Estatistica/MainWindow.xaml.cs
@@ -13,17 +13,18 @@ namespace Estatistica
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double _nextGauss;
-        private bool _available;
-        private Random _rng = new Random();
+        private double _nextGauss; // Z2 (será o próximo valor calculado por Z2=sqrt(-2 log u1)sin(2 pi u2) )
+        private bool _available; // Boolean que determina se proximo random pegará Z1 ou Z2
+        private Random _rng = new Random(); // Instância da classe Random principal (que gerará numeros da população)
 
-        int tamPopulacao = 1000;
-        int media = 12;
-        int variancia = 9;
-        double erro;
+        int tamPopulacao = 1000; // Tamanho da população
+        int media = 12; // Média dos elementos da pupulação
+        int variancia = 9; // Variância da população σ^2
+        int tamAmostra = 25; // Tamanho da amostra
+        double erro; // Erro padão da amostra
 
-        double[] populacao;
-        List<double[]> amostras;
+        double[] populacao; // População
+        List<double[]> amostras; // Lista com as Amostras
 
         public MainWindow()
         {
@@ -33,14 +34,28 @@ namespace Estatistica
             ChartArea aChartArea = new ChartArea("Intervalos");
             Chart1.ChartAreas.Add(aChartArea);
 
-            //Inicializando população
-            erro = 1.96 * (Math.Sqrt(variancia) / Math.Sqrt(25));
+            // Calcula erro com base de 95% de confiança
+            // 1,96 é valor de ... de acordo com z = 0,25 segundo tabela normal 
+            erro = 1.96 * (Math.Sqrt(variancia) / Math.Sqrt(tamAmostra)); 
+            // Inicializando população
             gerarPopulacao();
+            // Coleta Amostras
             coletarAmostras();
+            // Gera o Grafico
             gerarGrafico();
 
 
         }
+        
+        /// <summary>
+        /// Gera números aleatórios segundo expressao de Box-Muller
+        /// Z1 = sqrt(-2 log u1)cos(2 pi u2) )
+        /// Z2 = sqrt(-2 log u1)sin(2 pi u2) )
+        /// u1 e u2 são numeros aleatórios [0,1]
+        /// a cada chamada do método será retornado primeiro Z1 e depois Z2
+        /// <summary>
+        /// <param name="media"> Média populacional</param>
+        /// <param name="desvioPadrao"> Desvio padrão populacional</param>
         public double RandomGauss(double media, double desvioPadrao)
         {
             if (_available)
@@ -59,7 +74,9 @@ namespace Estatistica
             var toReturn = temp1 * Math.Cos(temp2);
             return media + desvioPadrao * toReturn;
         }
-
+        /// <summary>
+        /// Gera 20 amostras aleatórias de tamanho 25 com elementos da população
+        /// <summary>
         private void coletarAmostras()
         {
             amostras = new List<double[]>();
@@ -74,6 +91,9 @@ namespace Estatistica
                 for (int j = 0; j < 25; j++)
                     amostra[j] = populacao[numeros[j]];
                 amostras.Add(amostra);
+                
+                //------------------------------------------
+                // Atualiza interface
                 var item = new TreeViewItem()
                 {
                     Header = "Amostra " + (i + 1),
@@ -97,6 +117,8 @@ namespace Estatistica
             double erroSuperior = mediaAmostral + erro;
             double erroInferior = mediaAmostral - erro;
 
+            //------------------------------------------
+            // Atualiza interface
             StackPanelInformacoesAmostra.Children.Clear();
             StackPanelInformacoesAmostra.Children.Add(new TextBlock()
             {
@@ -121,6 +143,9 @@ namespace Estatistica
             }
             ListViewPopulação.ItemsSource = populacao;
             var mediaCalculada = calculaMedia(populacao);
+            
+             //------------------------------------------
+                // Atualiza interface
             StackPanelInformacoes.Children.Add(new TextBlock()
             {
                 Text = "Média da População: " + mediaCalculada
@@ -129,6 +154,7 @@ namespace Estatistica
 
         public void gerarGrafico()
         {
+            // Serie das amostras 
             Series errorBarSeries = new Series("ErrorBar")
             {
                 ChartType = SeriesChartType.ErrorBar,
@@ -136,9 +162,9 @@ namespace Estatistica
                 ShadowOffset = 1,
             };
             errorBarSeries["ErrorBarCenterMarkerStyle"] = "Circle";
-           // errorBarSeries["ErrorBarType"] = "StandardDeviation";
 
-
+            // Para cada amostra, gera uma barra 
+            // Calcula a média da amostra e seu intervalo de confiança
             for (int x = 1; x <= amostras.Count; x++)
             {
                 var amostra = amostras[x-1];
@@ -151,10 +177,8 @@ namespace Estatistica
                 errorBarSeries.Points.AddXY(x, mediaAmostral, erroInferior, erroSuperior);
 
             }
-
-            Chart1.Series.Clear();
-            Chart1.Series.Add(errorBarSeries);
-
+            
+            // Linha da média populacional
             StripLine stripline = new StripLine()
             {
                 Interval = 0,
@@ -164,10 +188,17 @@ namespace Estatistica
                 BorderDashStyle = ChartDashStyle.Dot,
                 Text = "μ"
             };
+            
+            Chart1.Series.Clear();
+            Chart1.Series.Add(errorBarSeries);
             Chart1.ChartAreas["Intervalos"].AxisY.StripLines.Add(stripline);
 
         }
-
+        
+        /// <summary>
+        /// Calcula média dos elementos de um array de double
+        /// <summary>
+        /// <param name="l"> Array de entrada </param>
         double calculaMedia(double[] l)
         {
             var media = 0.0;
